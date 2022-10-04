7c9d8e773bfaa82af0136850a7eedf01e506bf00
Colin Charleson <colin.charleson@ontariotechu.net>
2022-10-04 14:27:16 -0400

    plugin fix (week 3)

--- Assets/Plugin/SavePlugin.cs
+++ Assets/Plugin/SavePlugin.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System.Runtime.InteropServices;
+
 public class SavePlugin : MonoBehaviour
 {
     [DllImport("Plugin")]
@@ -30,31 +31,38 @@ public class SavePlugin : MonoBehaviour
     string m_Path;
     string fn;
 
-	private void Start()
-	{
+    // Start is called before the first frame update
+    void Start()
+    {
         inputAction = PlayerInputController.controller.inputAction;
 
         inputAction.Editor.Save.performed += cntxt => SaveItems();
 
         m_Path = Application.dataPath;
         fn = m_Path + "/save.txt";
-        Debug.Log(fn);
-	}
+        Debug.Log(fn);    
+    }
 
     void SaveItems()
-	{
+    {
         StartWriting(fn);
         foreach(GameObject obj in GameObject.FindGameObjectsWithTag("SpikeBall"))
-		{
-            if(obj.name.Contains("SpikyBall (1)"))
-			{
+        {
+            if(obj.name.Contains("SpikyBall1"))
+            {
                 SaveToFile(1, obj.transform.position.x, obj.transform.position.y, obj.transform.position.z);
-			}
+            }
             else
             {
                 SaveToFile(2, obj.transform.position.x, obj.transform.position.y, obj.transform.position.z);
             }
-		}
+        }
         EndWriting();
-	}
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
 }
